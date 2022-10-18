f0c760b944a694029f69e95ea21f867b3d9a5aea
leorivk <leorivk@gmail.com>
2022-10-18 21:01:37 +0900

    Fixed navigation bar | Update script.js

---
+++ sticky-navigation-bar/script.js
@@ -0,0 +1,21 @@
+const nav = document.querySelector("nav");
+const title = document.querySelector(".nav__title h1");
+const menu = document.querySelectorAll(".nav__menu li");
+
+const navHandler = () => {
+    if (window.scrollY <= 10) {
+        nav.style.background = "rgba(0, 0, 0, 0)";
+        title.style.color = "white";
+        for (i=0; i<menu.length; i++) {
+            menu[i].children[0].style.color = "white";
+        }
+    } else {
+        nav.style.background = "white";
+        title.style.color = "rgba(0, 0, 0, 0.7)";
+        for (i=0; i<menu.length; i++) {
+            menu[i].children[0].style.color = "rgba(0, 0, 0, 0.7)";
+        }
+    }
+}
+
+window.addEventListener("scroll", navHandler);
